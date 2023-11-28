57d9de9ffb25beea55aae9c359ba0a1e5387d2bc
Onkabetse02 <mogomotsionkabetse@gmail.com>
2023-11-28 14:20:35 -0800

    insert number

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -5,7 +5,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: integer
+ * @n: intege
  * @next: points to the next node
  *
  * Description: singly linked list node structure
@@ -14,7 +14,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
